8f10b2549d83e4b99c8d200263c4ac46030290e5
Dragon925 <dragon925.off@gmail.com>
2022-07-14 13:57:51 +0300

    Верстка старницы

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -2,10 +2,57 @@ package com.sirius.test_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.view.ContextThemeWrapper
+import com.google.android.material.chip.Chip
+import com.sirius.test_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val data = DataModel()
+
+        binding.tvName.text = data.name
+        binding.tvReviewCountSmall.text = data.gradeCnt
+        binding.tvDescription.text = data.description
+
+        with(binding.cgTagsList) {
+            removeAllViews()
+
+            addView(Chip(this@MainActivity, null, R.attr.tagChipStyle).also {
+                it.text = data.tags[0]
+            })
+
+            addView(Chip(this@MainActivity, null, R.attr.tagChipStyle).also {
+                it.text = data.tags[1]
+            })
+
+            addView(Chip(this@MainActivity, null, R.attr.tagChipStyle).also {
+                it.text = data.tags[2]
+            })
+        }
+
+
+        binding.tvRating.text = data.rating.toString()
+        val reviews = "${data.gradeCnt} Reviews"
+        binding.tvReviewCount.text = reviews
+
+        binding.tvReviewAuthor1.text = data.reviews[0].userName
+        binding.tvReviewDate1.text = data.reviews[0].date
+        binding.tvReview1.text = data.reviews[0].message
+
+        binding.tvReviewAuthor2.text = data.reviews[1].userName
+        binding.tvReviewDate2.text = data.reviews[1].date
+        binding.tvReview2.text = data.reviews[1].message
     }
 }
